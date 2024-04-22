314c497fbe937570078dc94e2a13b4712ed03a09
mgkagithub <65560010+mgkagithub@users.noreply.github.com>
2024-04-22 13:31:22 +0530

    update for the automation

--- autom.py
+++ autom.py
@@ -3,19 +3,33 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # Set up ChromeDriver
-chrome_driver_path = "C:\\Users\\omega\\OneDrive\\Desktop\\coding\\projects\\signez\\chromedriver.exe"  # Replace with your actual path
+chrome_driver_path = "C:\\Users\\omega\\OneDrive\\Desktop\\coding\\projects\\signez\\asl\\asl\\chromedriver.exe"  # Replace with your actual path
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
 # Open the website
-driver.get("")
-
+driver.get("https://mgkagithub.github.io/asl/")
+sleep(10)
 # Find elements by their XPath, ID, class, etc., and interact with them
-search_bar = driver.find_element_by_xpath("//input[@id='search-bar']")
-search_bar.send_keys("hello")
-search_bar.send_keys(Keys.RETURN)
+# search_bar = driver.find_element_by_xpath("/html/body/main/nav/a[2]/svg")
+# search_bar.send_keys("hello")
+# search_bar.send_keys(Keys.RETURN)
 
 # Click on buttons
-button1 = driver.find_element_by_xpath("//button[@id='button1']")
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[2]/svg")
+button1.click()
+time.sleep(2)
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[3]/svg")
+button1.click()
+time.sleep(2)
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[4]/svg")
+button1.click()
+time.sleep(2)
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[5]/svg")
+button1.click()
+time.sleep(2)
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[6]/svg")
+button1.click()
+time.sleep(2)
+button1 = driver.find_element_by_xpath("/html/body/main/nav/a[1]/svg")
 button1.click()
 
 # Add some waiting time to observe the actions
